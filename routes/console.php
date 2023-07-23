136c23fd21d0f1fa8b9c2c47dfdd7459ab71808b
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-07-23 17:31:21 +0700

    php artisan webmaster

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('webmaster', function () {
+    $this->comment("
+     ____                          _                                           
+    |  _ \ _   _ _______ _ __   __| | ___  ___ _   _        ___ ___  _ __ ___  
+    | |_) | | | |_  / _ \ '_ \ / _` |/ _ \/ __| | | |      / __/ _ \| '_ ` _ \ 
+    |  _ <| |_| |/ /  __/ | | | (_| |  __/\__ \ |_| |  _  | (_| (_) | | | | | |
+    |_| \_\\__, /___\___|_| |_|\__,_|\___||___/\__,_| (_)  \___\___/|_| |_| |_|
+           |___/                                                               
+    LARAVEL WEB BY SHIROKAMI RYZEN
+    ");
+})->purpose('Display an inspiring quote');
