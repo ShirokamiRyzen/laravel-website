907aad29e04e97e424fc386b6302a1a3a0dfd2da
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-30 10:55:40 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/privacy', function () {
 
 Route::get('/revanced', function () {
     return view('revanced', [
-        "direct_link" => "https://cloud.ryzendesu.com/api/raw/?path=/YT_ReVanced/youtube-revanced_v18.23.35.apk",
+        "direct_link" => "https://cloud.ryzendesu.com/api/raw/?path=/ReVanced/youtube/youtube-revanced_v18.23.35.apk",
         "mirror_1" => "https://github.com/ShirokamiRyzen/revanced-creator/releases/download/Youtube/youtube-revanced_v18.23.35.apk",
         "mirror_2" => "https://bayfiles.com/oaY6M43dze/youtube_revanced_v18_23_35_apk",
     ],[
@@ -44,7 +44,7 @@ Route::get('/revanced', function () {
 
 Route::get('/ytmusic', function () {
     return view('music', [
-        "direct_link" => "https://cloud.ryzendesu.com/api/raw/?path=/Music_ReVanced/youtube-music-revanced_v5.51.50.apk",
+        "direct_link" => "https://cloud.ryzendesu.com/api/raw/?path=/ReVanced/youtube_music/youtube-music-revanced_v5.51.50.apk",
         "mirror_1" => "https://github.com/ShirokamiRyzen/revanced-creator/releases/download/YT_Music/youtube-music-revanced_v5.51.50.apk",
         "mirror_2" => "https://bayfiles.com/Y8XaM534z8/youtube_music_revanced_v5_51_50_apk"
     ],[
@@ -58,7 +58,7 @@ Route::get('/ytmusic', function () {
 
 Route::get('/spotify', function () {
     return view('spotify', [
-        "direct_link" => "https://cloud.ryzendesu.com/api/raw/?path=/Spotify/Spotify%20v8.8.48.523%20(Merged).apk",
+        "direct_link" => "https://cloud.ryzendesu.com/api/raw/?path=/Spotify/Spotify.v8.8.48.523.Merged.apk",
         "mirror_1" => "https://github.com/ShirokamiRyzen/revanced-creator/releases/download/Spotify/Spotify.v8.8.48.523.Merged.apk",
         "mirror_2" => "https://bayfiles.com/i3YaMf38z5/Spotify_v8_8_48_523_Merged_apk"
     ],[
